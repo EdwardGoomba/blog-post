7eeff45d0aa0f1e61ac9f8c5a5b894e3123e2a45
EdwardGoomba <ask@edwardvdesign.com>
2018-01-03 11:32:48 -0500

    Added preview functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-$('#flashMessage').hide().fadeIn(1000).delay(3000).slideUp();
+// Hide alert message
+$('#flashMessage').hide();
+
+$('#previewButton').click(function() {
+  // Captures form title and content
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+
+  // Display form content in post preview
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').text(content);
+
+  $('#flashMessage').fadeIn(1000).delay(3000).slideUp();
+
+});
